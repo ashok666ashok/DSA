05ca1fd00fb1eb74a905b6ac664f45d9e0f5b6af
Ashok S <152708856+ashok666ashok@users.noreply.github.com>
2025-10-29 14:49:50 +0530

    Add isPrime function to check for prime numbers
    A simple solution is to iterate through all numbers from 2 to num-1 and for every number check if it divides num. If we find any number that divides, we return false.

---
+++ logic build problem/easy problems/python/isPrime.py
@@ -0,0 +1,10 @@
+def isPrime(num:int)->bool:
+    if num <= 1:
+        return False
+    else:
+        for i in range(2,num):
+            if num % i == 0:
+                return False
+        return True
+        
+print(isPrime(11))
