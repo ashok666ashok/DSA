cddeb021006e4e76a8ab7677f1691c8d6a9e25e4
Ashok S <152708856+ashok666ashok@users.noreply.github.com>
2025-10-29 13:09:27 +0530

    Delete easy problems directory

--- easy problems/python/sumofDigit.py
+++
@@ -1,9 +0,0 @@
-def sumOfDigits(num:int)->int:
-    sum = 0
-    while num!=0:
-        lastDigit = num%10
-        sum += lastDigit
-        num = num // 10
-    return sum
-    
-print(sumOfDigits(261))
